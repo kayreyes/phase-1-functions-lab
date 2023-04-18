435e7722823090460ec2e378593edeea88c04cf2
Kayla Reyes <kayla.reyes10.kr@gmail.com>
2023-04-17 20:55:18 -0500

    Completed phase-1-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+  var headquarters = 42; 
+  
+  return Math.abs(pickupLocation - headquarters); 
+}
+
+function distanceFromHqInFeet(blocks){
+    var blocksInFt = 264;
+return distanceFromHqInBlocks(blocks) * blocksInFt;
+}
+function distanceTravelledInFeet(start, destination) {
+    var blocksInFeet = 264; 
+    var distanceInBlocks = Math.abs(destination - start); 
+    var distanceInFeet = distanceInBlocks * blocksInFeet; 
+    
+    return distanceInFeet;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    var distanceInFeet = distanceTravelledInFeet(start, destination);
+    var farePrice;
+  
+    if (distanceInFeet <401) {
+      farePrice = 0;
+    } else if (distanceInFeet > 400 && distanceInFeet < 2001) {
+      farePrice = (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet < 2501) {
+      farePrice = 25;
+    } else if(distanceInFeet>2500) {
+      farePrice = 'cannot travel that far';
+    }
+  
+    return farePrice;
+  }
